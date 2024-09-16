7ccf29f1b7c4274b3903cab00ef60702a38bb12b
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-09-15 21:21:10 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -2,19 +2,19 @@
 $pageTitle = "Post result";
 include "view-header.php";
 ?>
-  
+
 <h1>Post result</h1>
 <?php
-echo getDisplay()
+echo getDisplay(); // Added missing semicolon
 
 include "view-footer.php";
 
 function getDisplay() {
   if (isset($_POST["my-name"])) {
-  return "<p>The value sent is </p>" . echo $_POST ["my-name"];
-} else {
-
- return "<p>Nothing posted to the page.</p>";
-}
+    // Return the full string with the value from POST
+    return "<p>The value sent is: " . htmlspecialchars($_POST["my-name"]) . "</p>"; 
+  } else {
+    return "<p>Nothing posted to the page.</p>";
+  }
 }
 ?>
